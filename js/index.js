87e3bb4406a96ac8864a7455de889f70c9467b25
bentongarrett <benton@bentongarrett.com>
2016-08-24 19:45:49 -0700

    Portfolio Redesign

---
+++ js/index.js
@@ -0,0 +1,23 @@
+$(".navbar-brand").click(function() {
+  $('html, body').animate({
+    scrollTop: $("body").offset().top
+  }, 1000);
+});
+
+$("#projects-link").click(function() {
+  $('html, body').animate({
+    scrollTop: $("#projects").offset().top
+  }, 1000);
+});
+
+$("#about-link").click(function() {
+  $('html, body').animate({
+    scrollTop: $("#about").offset().top
+  }, 1000);
+});
+
+$("#contact-link").click(function() {
+  $('html, body').animate({
+    scrollTop: $("#contact").offset().top
+  }, 1000);
+});
